c604ad7a231868db96d3094653fa232cbfb698b6
Tega Okorare <epicboy006@gmail.com>
2022-11-07 12:55:10 +0100

    Updated app to use products from the database

--- components/ProductItem.jsx
+++ components/ProductItem.jsx
@@ -2,26 +2,28 @@
 import React from "react";
 import Link from "next/link";
 
-function ProductItem({ product }) {
+function ProductItem({ product, addToCartHandler }) {
   return (
     <div className="card">
       <Link href={`/product/${product.slug}`}>
         <a>
-          <img
-            src={product.image}
-            alt={product.name}
-            className="shadow"
-          />
+          <img src={product.image} alt={product.name} className="shadow" />
         </a>
       </Link>
       <div className="flex flex-col items-center justify-center p-5">
         <Link href={`/product/${product.slug}`}>
-          <a><h2 className="text-lg">{product.name}</h2></a>
+          <a>
+            <h2 className="text-lg">{product.name}</h2>
+          </a>
         </Link>
         <p className="mb-2">{product.brand}</p>
         <p>${product.price}</p>
-        <button className="primary-button" type="button">
-            Add to Cart
+        <button
+          className="primary-button"
+          type="button"
+          onClick={() => addToCartHandler(product)}
+        >
+          Add to Cart
         </button>
       </div>
     </div>
